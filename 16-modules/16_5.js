523c54b955314a7e4981d6efc5ece261018b150b
Nikita <mail@mail.com>
2026-02-04 19:06:14 +0300

    modules

---
+++ 16-modules/16_5.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import { add, sub } from '../calc.js';
+
+const a = 2;
+console.log(sub(1, 3));
+console.log(add(1, 3));
+console.log(a)
---
+++ bundle.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+
+    function add(f, s) {
+        return f + s;
+    }
+
+    function sub(f, s) {
+        return f - s;
+    }
+
+    const a$1 = 1;
+    console.log(a$1);
+    // async function getProducts() {
+    //     const res = await fetch('https://dummyjson.com/products');
+    //     return res.json()
+    // }
+
+    // export const res = await getProducts();
+
+    // setTimeout(() => {
+    //     add = function (s, f) {
+    //         return f * s;
+    //     };
+    // }, 1500);
+
+    const a = 2;
+    console.log(sub(1, 3));
+    console.log(add(1, 3));
+    console.log(a);
+
+})();
